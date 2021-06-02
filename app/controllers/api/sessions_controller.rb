ab8a2941b46d5c3b9fd27029a4ed538ed29ad9a4
daniel-bogart <daniel.bogart17@gmail.com>
2021-06-02 12:52:37 -0700

    Created user/sessions controller, routes, jbuilder

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Api::SessionsController < ApplicationController
+  def create
+    # Find user by credentials
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ['Incorrect username or password'], status: 401
+    else
+      login!(@user)
+      render 'api/users/show';
+    end
+  end
+
+  def destroy
+    if @current_user
+    logout!
+    render json: { message: 'Logout successful!' }
+    else
+      render json: @user.errors.full_messages, status: 404
+  end
+end
