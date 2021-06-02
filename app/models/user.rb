f081cee80aa0474d0a5444971c04da25e5cd67c8
daniel-bogart <daniel.bogart17@gmail.com>
2021-06-01 17:15:55 -0700

    Added Heroku stuff

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password_digest, :first_name, :last_name, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password 
+
+  after_initialize :ensure_session_token
+
+
+      def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            return @user 
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64(64)
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64(64)
+    end
+end
