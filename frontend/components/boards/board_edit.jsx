8146663bd7c95830edc19aad98f6190ef17c5d91
daniel-bogart <daniel.bogart17@gmail.com>
2021-08-10 22:59:32 -0700

    Fixed board edit features

--- frontend/components/boards/board_edit.jsx
+++ frontend/components/boards/board_edit.jsx
@@ -7,7 +7,8 @@ class BoardEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
+      title: `${this.props.board.title}`,
+      description: `${this.props.board.description ||= ""}`,
       newModal: 'false'
     }
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,6 +17,7 @@ class BoardEdit extends React.Component {
   async handleSubmit(e) {
     e.preventDefault();
     await this.props.editBoard({title: this.state.title, id: this.props.boardId}, this.props.userId);
+    this.props.editBoard({description: this.state.description, id: this.props.boardId}, this.props.userId);
     this.props.closeModal();
   }
 
@@ -29,7 +31,8 @@ class BoardEdit extends React.Component {
 
   render() {
 
-    const title = this.props.board ? this.props.board.title : ""
+    // const title = this.props.board ? this.props.board.title : ""
+    // const description = this.props.board ? this.props.board.description : ""
 
     return (
       <div id="board-edit-container" className="board-edit-container-display">
@@ -39,14 +42,26 @@ class BoardEdit extends React.Component {
           <input
           type="text"
           value={this.state.title}
-          placeholder={`${title}`}
+          placeholder='Like "Places to Go" or "Recipes to Make"'
           onChange={this.handleInput('title')}
+          />
+          <h6>Description</h6>
+          <input
+          type="text"
+          value={this.state.description}
+          placeholder="What's your board about?"
+          onChange={this.handleInput('description')}
           />
         </div>
         <div className="final-board-edit-btn-container">
           <div onClick={this.handleSubmit} className="final-board-edit-btn">Done</div>
-          <div onClick={() => this.props.openModal('delete-board')} 
-            className="final-board-delete-btn">Delete</div>
+          <div className="delete-button-box" onClick={() => this.props.openModal('delete-board')}>
+            <div className="edit-board-delete-btn">Delete board</div>
+              <div className="delete-button-text">
+                <div>Delete this board and all its Pins forever.</div>
+                <div>You can't undo this!</div>
+              </div>
+          </div>
         </div>
       </div>
     )
