60c3e817484d3fd3d2a9d759a48556dbeffeac68
daniel-bogart <daniel.bogart17@gmail.com>
2021-10-08 23:04:42 -0700

    converted boardDelete to functional comp

--- frontend/components/boards/board_delete.jsx
+++ frontend/components/boards/board_delete.jsx
@@ -1,33 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-class BoardDelete extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
+const BoardDelete = (props) => {
 
-  async handleDelete(e) {
+  async function handleDelete(e) {
     e.preventDefault();
-    await this.props.deleteBoard(this.props.userId, this.props.boardId);
-    this.props.closeModal();
-    this.props.history.goBack();
+    await props.deleteBoard(props.userId, props.boardId);
+    props.closeModal();
+    props.history.goBack();
   }
 
-  render() {
 
-    return (
-      <div id="board-delete-container" className="board-delete-container-none">
-        <div>Are you sure?</div>
-        <div>Once you delete a board and all it's Pins, you can't undo it!</div>
-        <div className="final-board-delete-btn-container">
-          <div className="cancel-btn" onClick={() => this.props.openModal('edit-board')}>Cancel</div>
-          <div onClick={this.handleDelete} className="delete-btn">Delete Forever</div>
-        </div>
+  return (
+    <div id="board-delete-container" className="board-delete-container-none">
+      <div>Are you sure?</div>
+      <div>Once you delete a board and all it's Pins, you can't undo it!</div>
+      <div className="final-board-delete-btn-container">
+        <div className="cancel-btn" onClick={() => props.openModal('edit-board')}>Cancel</div>
+        <div onClick={handleDelete} className="delete-btn">Delete Forever</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default BoardDelete;
